a0876c8da663711143344743cced644f4033e2c2
Murat Bilginer <mrtblgnr@muratbilginer.net>
2016-04-23 00:18:53 +0300

    3 Kod-S CSharp Proje Basamak Sayısı Bul v1.0 Gloster
    Girilen bir sayının kaç basamaktan oluştuğunu söyleyen program.

---
+++ Kod-S Basamak Sayısı Bul v1.0 Gloster/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kod_S_Basamak_Sayısı_Bul_v1._0_Gloster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float sayi;
+            int basamak_sayisi = 1;
+
+            Console.Write("Sayiyi Giriniz=");
+
+            sayi = float.Parse(Console.ReadLine());
+
+            float sonuc = sayi;
+
+            while (sonuc >= 10)
+            {
+                basamak_sayisi++;
+                sonuc = sonuc / 10;
+
+            }
+
+            Console.WriteLine("Basamak Sayisi=" + basamak_sayisi);
+
+            Console.ReadKey();
+        }
+    }
+}
